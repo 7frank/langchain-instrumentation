2b779fc1a1fe081fc30a8c6675a851e042d8a179
Frank <frank.reimann@jambit.de>
2025-01-22 11:52:09 +0100

    add env PHONENIX_ARIZE_BASE_URL

--- index.ts
+++ index.ts
@@ -14,6 +14,8 @@ import { diag, DiagConsoleLogger, DiagLogLevel } from "@opentelemetry/api";
 import { LangChainInstrumentation } from "@arizeai/openinference-instrumentation-langchain";
 import * as CallbackManagerModule from "@langchain/core/callbacks/manager";
 
+const PHONENIX_ARIZE_BASE_URL =
+  process.env.PHONENIX_ARIZE_BASE_URL ?? "http://localhost:6006";
 
 export function autoInstrument() {
   // For troubleshooting, set the log level to DiagLogLevel.DEBUG
@@ -29,12 +31,11 @@ export function autoInstrument() {
   provider.addSpanProcessor(
     new SimpleSpanProcessor(
       new OTLPTraceExporter({
-        url: "http://localhost:6006/v1/traces",
+        url: `${PHONENIX_ARIZE_BASE_URL}/v1/traces`,
       })
     )
   );
   provider.register();
-
 
   const lcInstrumentation = new LangChainInstrumentation({});
   // LangChain must be manually instrumented as it doesn't have a traditional module structure
